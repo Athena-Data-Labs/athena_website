3f61c5e28bbc668a30389222a126fd597539a8e2
Vahidin Jupic <vahidinjupic@Vahidins-MacBook-Pro.local>
2026-06-30 10:51:16 -0400

    og image change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MotionConfig } from "framer-motion";
 import Index from "./pages/Index";
 import Labs from "./pages/Labs";
@@ -18,14 +18,14 @@ const App = () => (
         <Toaster />
         <Sonner />
         <MotionConfig reducedMotion="user">
-          <HashRouter>
+          <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/labs" element={<Labs />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </HashRouter>
+          </BrowserRouter>
         </MotionConfig>
       </TooltipProvider>
     </QueryClientProvider>
